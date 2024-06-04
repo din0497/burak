636fe11478928f1e96a020c098545c49a28abb4c
Din <rivojidin97@gmail.com>
2024-06-04 16:08:54 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+console.log("EXUCUTED!");
+
+import moment from 'moment'
+
+const current_time = moment().format("YYYY MM DD")
+
+const preson: string = "DIN";
+const age: number = 27;
+
+console.log(current_time);
+
