17b3fcc7299151a6fae5dfc34ad9a39fa070e33d
Din <rivojidin97@gmail.com>
2024-06-05 00:38:49 +0900

    feat: express intigration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config()
 import mongoose from "mongoose";
+import app from "./app";
 
 
-mongoose.connect(process.env.MONGO_URL as string, {}).then(data => { 
+mongoose.connect(process.env.MONGO_URL as string, {}).then(data => {
     console.log("MONGO-DB connectin succeed!");
     const PORT = process.env.PORT ?? 3003;
-    
+
+    app.listen(PORT, () => {
+        console.log(`The server is running on ${PORT}`);
+
+    })
+
 }).catch(err => {
     console.log("ERROR on connection MONGODB", err);
 
