2710a4bf76f04c1814cc888fba0442701b892496
Din <rivojidin97@gmail.com>
2024-06-08 22:50:42 +0900

    feat: solved H2-Task

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,24 @@
 /* 
-H-TASK: 
 
-shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
-MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+H2-TASK: 
+
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141"
+
+
 
 
 
 */
 
-const getPositive = (arr: number[]): string => {
-  return arr.filter(ele => ele >= 0).join('')
+const getDigits = (str: string): string => {
+  return str.split('')
+    .filter(ele => !isNaN(Number(ele)))
+    .join('')
+
 }
 
-const result = getPositive([1, -4, 2])
+const result = getDigits('m14i1t')
 
 console.log(result);
 
