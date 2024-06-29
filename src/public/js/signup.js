c1e6f2d673955f8691ce28b954f358d2939c5289
Din <rivojidin97@gmail.com>
2024-06-29 10:34:51 +0900

    feat: develop fontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,23 @@
 console.log("Signup frontend javascript file");
+
+const validateSignupForm = () => {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPassword === "" ||
+    confirmPassword === "" ||
+    memberPhone === ""
+  ) {
+    alert("Please insert all required inputs!");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert("The password is different,  please check!");
+    return false;
+  }
+};
