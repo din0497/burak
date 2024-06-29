4b265e5095bff42d43855ea72e9edb7e1bbfc41a
Din <rivojidin97@gmail.com>
2024-06-29 12:55:29 +0900

    feat: develop upload files front-end

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,33 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+
+  let fileName;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validationType = ["image/jpg", "image/jpeg", "image/png"];
+
+      if (!validationType.includes(fileType)) {
+        alert("Only image/jpg, image/jpeg, image/png");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        fileName = $(this)[0].files[0].name;
+      }
+
+      $(this).siblings(".upload-name").val(fileName);
+    }
+  });
+});
+
 const validateSignupForm = () => {
   const memberNick = $(".member-nick").val();
   const memberPhone = $(".member-phone").val();
@@ -20,4 +48,13 @@ const validateSignupForm = () => {
     alert("The password is different,  please check!");
     return false;
   }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+
+    if(!memberImage) {
+        alert("Please insert restaurant image")
+        return false
+    }
 };
