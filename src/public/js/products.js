adc796061cf277125452fb11a7ef33ecdbb5fa67
Din <rivojidin97@gmail.com>
2024-07-02 15:10:43 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -13,15 +13,36 @@ $(function () {
     }
   });
 
-  $('#process-btn').on('click', () => {
-    $('.dish-container').slideToggle(500)
-    $('#process-btn').css("display", "none")
-  })
-  $('#cancel-btn').on('click', () => {
-    
-    $('.dish-container').slideToggle(100)
-    $('#process-btn').css("display", "flex")
-  })
+  $("#process-btn").on("click", () => {
+    $(".dish-container").slideToggle(500);
+    $("#process-btn").css("display", "none");
+  });
+  $("#cancel-btn").on("click", () => {
+    $(".dish-container").slideToggle(100);
+    $("#process-btn").css("display", "flex");
+  });
+
+  $(".new-product-status").on("change", async (e) => {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    console.log("id", id);
+    console.log("status", productStatus);
+
+    try {
+      const response = await axios.post(`/admin/product/${id}`, {
+        productStatus: productStatus,
+      });
+      const result = response.data;
+      console.log("res", response.data);
+      if (result.data) {
+        console.log("Product updated!");
+        $(".new-product-status").blur();
+      } else alert("Product update failed!");
+    } catch (error) {
+      console.log(err);
+      alert("Update failed!");
+    }
+  });
 });
 
 const validateForm = () => {
