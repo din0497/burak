8e3a3b190973cae7187c1ed5fdd13a679bfac6ce
Din <rivojidin97@gmail.com>
2024-07-02 17:16:42 +0900

    feat: frontend updatechosen user

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,21 @@
 console.log("Users frontend javascript file");
+
+$(
+  $(".member-status").on("change", function (e) {
+    const id = e.target.id;
+    const status = e.target.value;
+
+    axios
+      .post(`/admin/user/edit`, {
+        _id: id,
+        memberStatus: status,
+        memberNick: "Usmon",
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+);
