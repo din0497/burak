4a9a033a90f716157c392d4338b5f9695f680279
Din <rivojidin97@gmail.com>
2024-07-23 21:18:36 +0900

    feat: Develop createOrder rest Api

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,26 @@
+import { ExtendedRequest } from "../libs/types/member";
+import { T } from "../libs/types/common";
+import { Response } from "express";
+import Errors, { HttpCode } from "../libs/Errors";
+import OrderService from "../models/Order.service";
+
+const orderService = new OrderService()
+
+const orderController: T = {};
+
+orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
+    try {
+        console.log("createOrder");
+
+        const result = await orderService.createOrder(req.member, req.body)
+
+        res.status(HttpCode.CREATED).json(result)
+    } catch (err) {
+        console.log("Error, createOrder", err);
+        if (err instanceof Errors) res.status(err.code).json(err)
+        else res.status(Errors.standard.code).json(Errors.standard)
+    }
+
+}
+
+export default orderController;
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from './controllers/member.controller';
 import uploader from './libs/utils/uploader';
 import productController from './controllers/product.controller';
+import orderController from './controllers/order.controller';
 
 
 /* Member */
@@ -34,6 +35,10 @@ router.get("/product/all", productController.getProducts)
 router.get("/product/:id", memberController.retrieveAuth, productController.getProduct)
 
 /* Orders */
+
+router.post("/order/create",
+     memberController.verifyAuth, 
+     orderController.createOrder)
 
 
 export default router
