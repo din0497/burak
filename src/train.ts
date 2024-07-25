81aa6c27a67426428245ea7fc153710ec71fde05
Din <rivojidin97@gmail.com>
2024-07-26 04:25:32 +0900

    feat: Z-TASK solved

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,24 @@
 /* 
-Y-TASK:
+Z-TASK:
 
- Shunday function yozing, uni 2 ta array parapetri bolsin. Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda qaytarsin
- MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
+Shunday function yozing, uni sonlardan tashkil topgan array qabul qilsin. Function arraydagi juft sonlarni yigindisini qaytarsin
+MASALAN: sumEvens([1,2,3]) return 2
 
 */
-/* const findIntersection = (arr1: number[], arr2: number[]): number[] => {
-  const result = []
 
-  for (const num of arr1) {
-    if (arr2.includes(num)) {
-      result.push(num)
+const sumEvens = (arr: number[]): number => {
+  let sum = 0;
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      sum += num
     }
   }
-  return result
+
+
+  return sum
 }
 
-
-console.log(findIntersection([1, 2, 3, 0], [3, 2, 0])); */
+console.log(sumEvens([1, 2, 3, 4, 5, 6, 7, 8, 10]));
 
 
 
@@ -38,4 +39,5 @@ console.log(findIntersection([1, 2, 3, 0], [3, 2, 0])); */
 */
 
 
-const students = ["Max"]
+
+
