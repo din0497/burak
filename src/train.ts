67383e6ace8b3675c61ef8ffa9f6943f1498629e
Din <rivojidin97@gmail.com>
2024-07-30 05:22:08 +0900

    feat: Tasl ZC solved

--- src/train.ts
+++ src/train.ts
@@ -1,34 +1,18 @@
 /* 
-ZA-TASK:
+ZC-TASK:
 
-Shunday function yozing, u array ichidagi objectlarni “age” qiymati boyicha sortlab bersin. 
-MASALAN: sortByAge([{age:23}, {age:21}, {age:13}]) return [{age:13}, {age:21}, {age:23}]
+Shunday function yozing, uni number parametri bolsin va function qabul parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin
+MASALAN: celsiusToFahrenheit(0) return 32
 
 */
-interface T {
-  age: number
-}
-const sortAge = (arr: T[]): T[] => {
 
-  return arr.sort((a, b) => a.age - b.age)
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9 / 5) + 32;
+  
 }
 
 
-console.log(sortAge([{ age: 23 }, { age: 21 }, { age: 13 }]));
-
-
-/* 
-ZB-TASK:
-
-Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
-MASALAN: randomBetween(30, 50) return 45
-*/
-
-const randomBetween = (min: number, max: number): number => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-console.log(randomBetween(20, 50));
+console.log(celsiusToFahrenheit(0));
 
 
 /* Project Standards
