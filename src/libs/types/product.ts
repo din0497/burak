b3b174819394680343cf6b2f0a200c81303e0105
Din <rivojidin97@gmail.com>
2024-08-02 19:58:50 +0900

    feat: CORS integration

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -13,6 +13,8 @@ export interface Product {
     productDesc: string;
     productImages: string[];
     productViews: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 export interface ProductInquiry {
