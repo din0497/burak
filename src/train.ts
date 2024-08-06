b7ee60961e6d963a1106da52feb4a36687089609
Din <rivojidin97@gmail.com>
2024-08-07 02:46:07 +0900

    feat: task ZF solved

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,23 @@
 /* 
-ZE-TASK:
+ZF-TASK:
 
-Shunday function yozing, uni  string parametri bolsin. String ichida takrorlangan harflarni olib tashlab qolganini qaytarsin
-MASALAN: removeDuplicate('stringg') return 'string'
-
+Shunday function yozing, uni string parametri bolsin. String ichidagi har bir sozni bosh harflarini katta harf qilib qaytarsin lekin 1 yoki 2 harfdan iborat sozlarni esa oz holicha qoldirsin.
+MASALAN: capitalizeWords('name should be a string') return 'Name Should be a String'
 */
-
-const removeDuplicate = (str: string) => {
- return [...new Set(str)].join('')
+function capitalizeFirstChar(str: string) {
+    if (str.length <= 2) {
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-
-console.log(removeDuplicate('stringg'));
+const capitalizeWords = (str: string) => {
+    return str.split(' ')
+        .map(
+            ele => capitalizeFirstChar(ele))
+        .join(' ')
+}
+console.log(capitalizeWords("name should be a string"));
 
 
 /* Project Standards
